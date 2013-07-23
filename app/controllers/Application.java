0946a9f7455cadc02cfecb6877d09a5afc53743c
Kristian Haugene <kristian.haugene@capgemini.com>
2013-07-23 23:53:50 +0200

    html for login. better main.html

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,14 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+
+    /**
+     * Login page.
+     */
+    public static Result login() {
+        return ok(
+                login.render()
+        );
+    }
   
 }
